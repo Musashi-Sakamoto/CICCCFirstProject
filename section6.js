a691a622679e94a13a43b078eb9d068c4b1c395b
Musashi Sakamoto <1292602b@gmail.com>
2020-05-11 23:46:02 -0700

    imediately invoked function

--- section6.js
+++ section6.js
@@ -1,23 +1,25 @@
-const number6Input = document.getElementById("input6");
-const answer6Text = document.getElementById("answer6");
-
-number6Input.addEventListener('input', function(e) {
-  answer6Text.innerHTML = fibonacciNums(parseInt(e.target.value));
-})
-
-function fibonacciNums(idx) {
-  let pre = 1;
-  let cur = 1;
-  const returnedArray = [];
-  for (let i = 0; i < idx; i++) {
-    if (i === 0 || i === 1) {
-      returnedArray.push(1);
-    } else {
-      const current = pre + cur;
-      returnedArray.push(current);
-      pre = cur;
-      cur = current;
+(function() {
+  const numberInput = document.getElementById("input6");
+  const answerText = document.getElementById("answer6");
+  
+  numberInput.addEventListener('input', function(e) {
+    answerText.innerHTML = fibonacciNumbers(parseInt(e.target.value));
+  })
+  
+  function fibonacciNumbers(idx) {
+    let pre = 1;
+    let cur = 1;
+    const returnedArray = [];
+    for (let i = 0; i < idx; i++) {
+      if (i === 0 || i === 1) {
+        returnedArray.push(1);
+      } else {
+        const current = pre + cur;
+        returnedArray.push(current);
+        pre = cur;
+        cur = current;
+      }
     }
+    return returnedArray;
   }
-  return returnedArray;
-}
+})();
