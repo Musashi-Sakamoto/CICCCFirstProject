6a41c966cbba6a6ea690b6f28298fd0dc2877a74
Musashi Sakamoto <1292602b@gmail.com>
2020-05-11 19:23:06 -0700

    section 5

--- section5.js
+++ section5.js
@@ -1,3 +1,10 @@
+const numberInput = document.getElementById("input5");
+const answerText = document.getElementById("answer5");
+
+numberInput.addEventListener('input', function(e) {
+  answerText.innerHTML = isPrime(parseInt(e.target.value)) ? 'It is a prime number' : 'It is not a prime number';
+})
+
 function isPrime(numInput) {
   if (numInput === 1) return false;
   if (numInput === 2) return true;
