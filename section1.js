5ee94d12706f2735cd0de028dab851ddecb73ca4
ajayreddy-1 <65198862+ajayreddy-1@users.noreply.github.com>
2020-05-12 15:53:50 -0700

    section1

--- section1.js
+++ section1.js
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>section1</title>
-    <script src ="section1.js"></script>
-</head>
-<body>
-    <p id="test"></p>
-    <script>
-        const name = 'Ajay'
-        console.log(`Hi ${name}, Welcome to this page`);
-        document.getElementById('test').innerHTML = `Hi ${name}, Welcome to this page`;
-    </script>
-</body>
-</html>
+(function() {
+  const numberInput = document.getElementById("input1");
+  const answerText = document.getElementById("answer1");
+
+  numberInput.addEventListener('input', function(e) {
+    answerText.innerHTML = `Hi ${e.target.value}, Welcome to this page`
+  })
+})();
