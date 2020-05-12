fc56fe63b8b82c079fd6f224742af7c057b10831
Musashi Sakamoto <1292602b@gmail.com>
2020-05-12 10:41:31 -0700

    examples for section 3 and 4

--- age.js
+++ age.js
@@ -1,11 +1,18 @@
-let age= prompt("please enter your age: ");
-let response;
-if (age <=20) {
-response='You need to study hard';}
-else if (age<=30){ 
-response='Study more';}
-else if (age<=40) {response='Make a lot of money';} 
-else if (age<=50) {response='Be successful';} 
-else response='Travel a lot'
+(function() {
+  const numberInput = document.getElementById("input3");
+  const answerText = document.getElementById("answer3");
 
-document.getElementById("age").innerHTML=response
+  numberInput.addEventListener('input', function(e) {
+    const age = numberInput.value;
+    let response;
+    if (age <=20) {
+    response='You need to study hard';}
+    else if (age<=30){ 
+    response='Study more';}
+    else if (age<=40) {response='Make a lot of money';} 
+    else if (age<=50) {response='Be successful';} 
+    else response='Travel a lot'
+    
+    answerText.innerHTML=response
+  })
+})();
--- sum.js
+++ sum.js
@@ -1,16 +1,27 @@
-let number1=parseInt(prompt("please enter the first number :"));
-let number2=parseInt(prompt("please enter the second number :"));
 
-if (isNaN(number1) || isNaN(number2)) {
-    ///document.getElementById("sum1").innerHTML= "Input not valid";
-    document.alert("Input not valid");
-  }
-let sum=0;
-let number=0;
-if (number2<number1) { number=number1; number1=number2; number2=number;}
-for (let index = number1; index <= number2 ; index++) {
-   sum +=index ;    
-}
+(function() {
+  const numberInput1 = document.getElementById("input4first");
+  const numberInput2 = document.getElementById("input4second");
+  const button = document.getElementById("button4");
+  const answerText = document.getElementById("answer4");
 
-document.getElementById("sum1").innerHTML=sum;
-console.log(sum);
+  button.addEventListener('click', function(e) {
+    const number1 = parseInt(numberInput1.value);
+    const number2 = parseInt(numberInput2.value);
+    console.log(number1, number2);
+
+    if (isNaN(number1) || isNaN(number2)) {
+      ///document.getElementById("sum1").innerHTML= "Input not valid";
+      alert("Input not valid");
+    }
+    let sum=0;
+    let number=0;
+    if (number2<number1) { number=number1; number1=number2; number2=number;}
+    for (let index = number1; index <= number2 ; index++) {
+      console.log('index: ', index);
+      sum +=index ;    
+    }
+
+    answerText.innerHTML=sum;
+  });
+})();
