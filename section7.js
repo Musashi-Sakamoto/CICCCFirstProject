9c1e7c9ca23ee92144d735af8c41b353dceb20ff
Mahsa Maleki <mahsamaleki088@gmail.com>
2020-05-12 13:02:13 -0700

    fix section 7

--- section7.js
+++ section7.js
@@ -9,8 +9,7 @@
     var date_diff_indays = function(birthDate) {
         if (birthDate.length == 10) {
         dt1 = new Date(birthDate);
-        dt2 = new Date.now();
-        alert(typeof(dt2));
+        dt2 = new Date(Date.now());
           return 'You have lived for ' +  Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24)) + ' Days';
         }
         else {return 'Input Invalid'}
