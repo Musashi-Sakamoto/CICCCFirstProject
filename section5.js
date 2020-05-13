9318a36c27c7e99a1b2ef306a7b01f0eb8572a77
Musashi Sakamoto <1292602b@gmail.com>
2020-05-12 17:39:08 -0700

    validation for numbers less than or equal to 1

--- section5.js
+++ section5.js
@@ -3,17 +3,15 @@
   const answerText = document.getElementById("answer5");
 
   numberInput.addEventListener('input', function(e) {
-    answerText.innerHTML = isPrime(parseInt(e.target.value));
+    answerText.innerHTML = isPrime(parseInt(e.target.value)) ? 'It is a prime number' : 'It is not a prime number';
   })
-let prime='It is a prime number';
-let notPrime='It is not a prime number';
+
   function isPrime(numInput) {
-    if (numInput <0) return'it is invalid number';
-    if (numInput === 1) return notPrime;
-    if (numInput === 2) return prime;
+    if (numInput <= 1) return false;
+    if (numInput === 2) return true;
     for (let i = 2; i < numInput; i++) {
-      if (numInput % i === 0) return notPrime;
+      if (numInput % i === 0) return false;
     }
-    return prime;
+    return true;
   }
 })();
